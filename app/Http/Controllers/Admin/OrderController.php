e68fefc8805b76b6dbe3783f6578c717105cd386
dhanikwp <dhanikusuma256@gmail.com>
2026-07-05 14:35:49 +0700

    fix: Add missing Carbon import in OrderController

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\OrderConfirmedMail;
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
